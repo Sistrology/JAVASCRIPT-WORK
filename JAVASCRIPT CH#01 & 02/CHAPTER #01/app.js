6f064d6ee8d8e4377e8a40bd8390be012b3116fc
Syeda fizza attaria <Syedafizanaz497@gmail.com>
2024-10-16 07:52:18 +0500

    Add files via upload

---
+++ JAVASCRIPT CH#01 & 02/CHAPTER #01/app.js
@@ -0,0 +1,19 @@
+// ANSWER NUMBER # 01. 
+//Alert hamay browser per popp message alert kr kay dyta hai
+// Alert ka syntax hai
+// alert();
+// alert ko lower case ma hi likhty hain
+// ANSWER NUMBER # 02. 
+// alert("Welcome to my website!.");
+// ANSWER NUMBER # 03.
+ // Agr capital letter ma likhygy ya first letter capital to error ayega.
+// ANSWER NUMBER # 04 .
+ // alert("Hello, world!")
+// ANSWER NUMBER # 05 . 
+// alert ka pura syntax yeh hai
+// alert( ) :
+// alert keyword hai
+// ( ) isky andar jo hamnay show krwana hota hay osay likhty hai string may ya number may
+// and last semicolon ;
+// Is k baghair bhi code run hojata ha lekin isay lagana zarori hay
+
---
+++ JAVASCRIPT CH#01 & 02/CHAPTER #02/app.js
@@ -0,0 +1,21 @@
+// Answer No : 1
+// var  message = "Hello, world!"
+// alert(message);
+// Answer No : 2 
+// var age = 25;
+// alert(age);
+//Answer No : 3 
+// Declare a variable named 'greeting' and assign it the value "Good morning!"
+// var greeting = "Good morning!";
+// // Display the initial value of greeting in an alert
+// alert(greeting);
+// // Change the value of greeting to "Good evening!"
+//  var greeting = "Good evening!";
+// // Display the new value of greeting in another alert
+// alert(greeting);
+// Answer No : 4 
+// var favoriteColor = " Blue";
+// alert("My favorite color is" +  favoriteColor);
+// Answer No : 5 
+// var age = 25;
+// alert(age);
